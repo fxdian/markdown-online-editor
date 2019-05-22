6e331265b672243ca8739a4a13fc1330e7afb501
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2019-05-22 22:27:22 +0800

    🎉 update release

---
+++ precache-manifest.e92fba9ec0d3c1ce7c2fc39a9c5db6c2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "316170b772ca0e1467e2",
+    "url": "/js/chunk-7cd940b5.9c9d94b7.js"
+  },
+  {
+    "revision": "369f6129748a531567ab3535689be69a",
+    "url": "/site.webmanifest"
+  },
+  {
+    "revision": "3fc535fca65a6102eb46",
+    "url": "/js/chunk-a6543f6e.624b9c51.js"
+  },
+  {
+    "revision": "3bae421aac706da95e92",
+    "url": "/css/chunk-2121a7bb.6d6756b8.css"
+  },
+  {
+    "revision": "96de62ca1deb2c57319c",
+    "url": "/js/chunk-37a58f5b.9fcf63a9.js"
+  },
+  {
+    "revision": "0553cdaefaad8055812d",
+    "url": "/css/chunk-77294a72.d10d84c3.css"
+  },
+  {
+    "revision": "0553cdaefaad8055812d",
+    "url": "/js/chunk-77294a72.e0799b4b.js"
+  },
+  {
+    "revision": "316170b772ca0e1467e2",
+    "url": "/css/chunk-7cd940b5.d67a4b87.css"
+  },
+  {
+    "revision": "28698f41f3ad185da432",
+    "url": "/js/app.596b75ed.js"
+  },
+  {
+    "revision": "3bae421aac706da95e92",
+    "url": "/js/chunk-2121a7bb.29c74da5.js"
+  },
+  {
+    "revision": "6520c81ed1a49f5026a1",
+    "url": "/js/element.8f5126b7.js"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "191f8221d959cad6fcafa244934c6e1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bf35c3805e83f0178f9e224d8d0febca",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "28698f41f3ad185da432",
+    "url": "/css/app.2629eb4e.css"
+  }
+];
